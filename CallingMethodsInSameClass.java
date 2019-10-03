3154781ffab7a25127eed43bcee7c81bfae5ebaf
Chaitanya Senapati <csenapati12@gmail.com>
2019-10-03 21:33:45 +0530

    Create CallingMethodsInSameClass.java

---
+++ CallingMethodsInSameClass.java
@@ -0,0 +1,18 @@
+public class CallingMethodsInSameClass
+{
+	public static void main(String[] args) {
+		CallingMethodsInSameClass as = new CallingMethodsInSameClass();
+		int addValue =as.add(1004556,1000);
+		System.out.println("Adding values1111      "+addValue);
+	}
+
+	public int add(int a, int b) {
+	int c ;
+	
+	c= a+b;
+	System.out.println("Adding values     "+c);
+	return c;
+	}
+
+	
+}
